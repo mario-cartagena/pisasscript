d165da052f25b9f45dc78cf9698aeb410aa435ad
mario-cartagena <jhonmariocartagena@gmail.com>
2023-06-10 17:36:51 -0500

    changes in search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,32 +3,33 @@ import '../components/search/styleSearch.scss'
 import IconPizza from '../assets/icons/pizza.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Carousel, Form } from 'react-bootstrap';
 import { getPizzas } from '../services/getPizzas';
-
+import { NavLink } from 'react-router-dom';
 
 const Search = () => {
-
   const [pizzas, setPizzas] = useState([]);
   const [searchPizzas, setSearchPizzas] = useState("");
-
+  const [dataPizzas, setDataPizzas] = useState([]); // Agregado
 
   useEffect(() => {
-    getPizzas().then((response)=>{
-      console.log(response)
-      setPizzas(response)
-    })
-  }, [])
+    getPizzas().then((response) => {
+      console.log(response);
+      setPizzas(response);
+    });
+  }, []);
 
   const handleSearchPizza = (event) => {
     event.preventDefault();
     setSearchPizzas(event.target.value);
-    console.log(searchPizzas)
+    console.log(searchPizzas);
+
     const filteredPizzas = pizzas.filter((pizza) =>
-      pizza.name.toLowerCase().includes(searchPizzas.toLowerCase())
-    );
-    console.log(filteredPizzas)
-  }
+        pizza.name.toLowerCase().includes(searchPizzas.toLowerCase())
+      )
+    console.log(filteredPizzas);
+    setDataPizzas(filteredPizzas)
+  };
   return (
     <div className='search'>
       <div className='search__header'>
@@ -39,16 +40,18 @@ const Search = () => {
         <div className='search__image'></div>
       </div>
       <div className="search__bar">
-        <Form className="search__form"  onSubmit={handleSearchPizza} >
-            <Form.Control
-              type="text"
-              placeholder="Pizza de peperoni, mexicana, hawaiana..."
-              className="me-2 search__input"
-              aria-label="Search"
-              onChange={handleSearchPizza}
-            />
-          <Button type="submit" variant="outline-success" className='search__button'><FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#d0758e", height: "65%"}} /></Button>
-        </Form>  
+        <Form className="search__form" onSubmit={handleSearchPizza}>
+          <Form.Control
+            type="text"
+            placeholder="Pizza de peperoni, mexicana, hawaiana..."
+            className="me-2 search__input"
+            aria-label="Search"
+            onChange={handleSearchPizza}
+          />
+          <Button type="submit" variant="outline-success" className='search__button'>
+            <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: "#d0758e", height: "65%" }} />
+          </Button>
+        </Form>
       </div>
       <div className="search__pizza">
         <figure className='search__figure'>
@@ -60,4 +63,4 @@ const Search = () => {
   )
 }
 
-export default Search
+export default Search;
